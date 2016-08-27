8733ac35e1fffe5da0dc873d947f4b3b9441e3c8
terrantian <terran.tian@foxmail.com>
2016-08-27 11:12:27 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # tiny.lua
+
+An tool to encode/decode a unsigned number to a string. Highly inspired by [tiny](https://github.com/kylebragger/tiny)
+
+### demo code
+```lua
+print(tiny.tonumber(2834438,2));
+print(tiny.tonumber(2834438,8));
+print(tiny.tonumber(2834438,16));
+print(tiny.tonumber(2834438,33));
+print(tiny.tonumber(2834438,61));
+
+```
+the result is:
+```lua
+1010110100000000000110
+12640006
+2B4006
+2CSQ2
+CTjC
+```
+## set_base(value)
+
+Set the default letters which will be used to do encode/decode, the default is 
+
+    0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_
+
+you can reorder these charactors for self format purpose.
+
+## tonumber(e,[base])
+
+similar with gloable function tonumber,but can use different letters set. 
+An optional argument specifies the base to interpret the numeral. The base may be any integer between 2 and the length of the letters set. each letter represents the index of it in the charactors. for use the default letters set，"A" represents 10,"a" represents 37.
--- tiny.lua
+++ tiny.lua
@@ -1,24 +1,49 @@
 	local M = {};
 
-	local bases_map = {}
-	bases_map["2"] = "01"
-	bases_map["8"] = "01234567"
-	bases_map["10"] = "0123456789"
-	bases_map["16"] = "0123456789ABCDEF"
-	bases_map["36"] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	bases_map["62"] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	local base_set= "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_"
 
-	-- index range [0,len-1]
-	function string.charAt(self,index)
-		return self:sub(index+1,index+1) 
+	function M.set_base(value) 
+		base_set = value or base_set;
 	end
 
-	-- index range [-1,len-1]
-	-- -1 returned if not find.
-	function string.index(self,str)
-		local index,_ = self:find(str);
-		index = index or 0;
-		return index - 1
+	function M.tonumber(e,base)
+		base = base or 10;
+		if base == 10 then return  tonumber(e) end
+		if tonumber(e) ~= nil then return M.encode(e,base) end
+
+		return M.decode(e,base);
+	end
+
+	function M.encode(interger,base)
+		base = base or 62;
+		local key = base_set:sub(1,base);
+		local hexn,radix = "",key:len();
+		if base <=36 then key:lower();end
+		
+		repeat
+			local r = interger % radix;
+			interger = (interger - r) / radix
+			hexn = key:charAt(r) .. hexn;
+		until interger == 0
+		
+		return  hexn
+	end
+
+	function M.decode(string,base)
+		base = base or 62;
+		local key = base_set:sub(1,base);
+		local radix,strlen = key:len(),string:len();
+		if base <=36 then key:lower();string:lower() end
+
+		local n,i = 0,0;
+		while i < strlen do
+			local p = key:index(string:charAt(i));
+			if p < 0 then return -1 end
+			n =n + p * math.pow(radix, strlen - i - 1);
+			i = i+1;
+		end
+
+		return n;
 	end
 
 	function string.fromhex(self)
@@ -33,37 +58,24 @@
 	    end))
 	end
 
-	function M.regiest(base,key)
-		bases_map[base..""] = key;
+	-- index range [0,len-1]
+	function string.charAt(self,index)
+		return self:sub(index+1,index+1)
 	end
 
-	function M.encode(interger,base)
-		local key = bases_map[tostring(base or 62)]
-		local hexn,radix = "",key:len();
-		
-		repeat
-			local r = interger % radix;
-			interger = (interger - r) / radix
-			hexn = key:charAt(r) .. hexn;
-		until interger == 0
-		
-		return  hexn
+	-- index range [-1,len-1]
+	-- -1 returned if not find.
+	function string.index(self,str)
+		local index,_ = self:find(str);
+		index = index or 0;
+		return index - 1
 	end
 
-	function M.decode(string,base)
-		local key = bases_map[tostring(base or 62)]
-		local radix,strlen = key:len(),string:len();
-
-		local n,i = 0,0;
-		while i < strlen do
-			local p = key:index(string:charAt(i));
-			if p < 0 then return -1 end
-			n =n + p * math.pow(radix, strlen - i - 1);
-			i = i+1;
-		end
-
-		return n;
-	end
+	print(M.tonumber(2834438,2));
+	print(M.tonumber(2834438,8));
+	print(M.tonumber(2834438,16));
+	print(M.tonumber(2834438,33));
+	print(M.tonumber(2834438,61));
 
 	print(M.decode(M.encode(288,2),2));
 	print(M.decode(M.encode(288)))
